00c55451a839253a71057b6c97110cbff198312b
82107 <choiday0815@gmail.com>
2020-06-18 11:26:54 +0900

    upload

---
+++ practice.py
@@ -0,0 +1,17 @@
+#문자열 자료형
+print("ㅋ"*9)
+# boolean 자료형 참 /거짓
+print(not True)
+print(not False)
+print(not(5>10))
+#변수-어떠한 값을 저장하는 공간
+animal="강아지"
+name="연탄이"
+age=4
+hobby="산책"
+is_adult= age>=3
+print("우리집"+animal+"의 이름은"+name+"에요")
+print(name+"는"+str(age)+"살이며"+hobby+"을 아주 좋아해요")
+print(name,"는", (age), "살이며", hobby, "을 아주 좋아해요")
+print(name+"는 어른일까요"+str(is_adult))
+
